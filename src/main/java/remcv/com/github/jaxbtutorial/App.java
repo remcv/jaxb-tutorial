b3c43928398619ec5e28d3fb2fc90140eaf7d687
remcv <remuluscatana@tutanota.com>
2021-08-27 08:59:02 +0300

    Output XML to an OutputStream

--- src/main/java/remcv/com/github/jaxbtutorial/App.java
+++ src/main/java/remcv/com/github/jaxbtutorial/App.java
@@ -6,27 +6,35 @@ import jakarta.xml.bind.Marshaller;
 import remcv.com.github.jaxbtutorial.model.Book;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.OutputStream;
 import java.util.Date;
 
 public class App {
 
     // methods
     public static void main(String[] args) throws JAXBException {
-        Book book1 = new Book(1L, "Morometii", "Marin Preda", null);
+        Book book1 = new Book(1L, "Elementary statistics", "Mario Triola", new Date());
         Book book2 = new Book(2L, "Fundamentals of Neurology", "Mattle Mumenthaler", new Date());
 
-
-        // marshal a Book instance
+        // marshal Book instances
         marshalObject(book1, "./book1.xml");
-        marshalObject(book2, "./book2.xml");
+        marshalObject(book2, System.out);
     }
 
+    // marshal an object to a file
     private static <T> void marshalObject(T object, String filePath) throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
         Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(object, new File(filePath));
+    }
+
+    // marshal an object to a Java IO output stream
+    private static <T> void marshalObject(T object, OutputStream out) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(object, out);
     }
 
 }
